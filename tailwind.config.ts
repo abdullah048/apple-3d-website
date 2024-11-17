5e1d7b58741f6ca604813b91a1fc51733e4a8736
abdullah048 <abdullahshahbaz048@gmail.com>
2024-11-17 20:17:35 +0500

    doc: setting up dummy data and default colors and assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        blue: '#2997FF',
+        gray: {
+          DEFAULT: '#86868b',
+          100: '#94928d',
+          200: '#afafaf',
+          300: '#42424570',
+        },
+        zinc: '#101010',
       },
     },
   },
